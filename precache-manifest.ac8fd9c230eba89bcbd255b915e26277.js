5d138a49991f1e55ea4eeacca74c2828ba078927
barema4 <samrubarema6@gmail.com>
2020-08-10 12:25:31 +0300

    Updates

---
+++ precache-manifest.ac8fd9c230eba89bcbd255b915e26277.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d8cd55dd9b4f64b2f9a237bdc3b78c5",
+    "url": "/porti/index.html"
+  },
+  {
+    "revision": "a4431fe5e69e3f837f36",
+    "url": "/porti/static/css/3.f9898e9a.chunk.css"
+  },
+  {
+    "revision": "a28d7f5249452652582d",
+    "url": "/porti/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "2b4b1f237ccf91184f7f",
+    "url": "/porti/static/css/main.ec7baffb.chunk.css"
+  },
+  {
+    "revision": "59d52bc3b7e59fb88170",
+    "url": "/porti/static/js/2.cadd4f1f.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/porti/static/js/2.cadd4f1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4431fe5e69e3f837f36",
+    "url": "/porti/static/js/3.b35c32bd.chunk.js"
+  },
+  {
+    "revision": "a28d7f5249452652582d",
+    "url": "/porti/static/js/4.974a26da.chunk.js"
+  },
+  {
+    "revision": "2b4b1f237ccf91184f7f",
+    "url": "/porti/static/js/main.f38d9797.chunk.js"
+  },
+  {
+    "revision": "44646c9f33964afae2ce",
+    "url": "/porti/static/js/runtime-main.c13e5b3b.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/porti/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "eafb85eb0fa4e7e3c8915378b4483832",
+    "url": "/porti/static/media/Andela.eafb85eb.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/porti/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/porti/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "74a4f3dcbeaf6e531223588cf73b6950",
+    "url": "/porti/static/media/cowsoko.74a4f3dc.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/porti/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "b627c48df76e7df1928b14cc0279ff53",
+    "url": "/porti/static/media/freelance.b627c48d.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/porti/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/porti/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/porti/static/media/skill.34409398.svg"
+  }
+]);
